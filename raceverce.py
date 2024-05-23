4a689576193b626a50ce48b545b92539d74d03bf
Nazir <rtg25g@gmail.com>
2024-05-23 17:36:54 +0330

    chanches for

--- raceverce.py
+++ raceverce.py
@@ -1,6 +1,7 @@
 word = "Hello "
 next = "amir welcome to repository"
 print(word + next)
-user = int(input("Enter your number"))
-for user in range(12):
+user = int(input("Enter your number: "))
+for user in range(1, user + 1):
+  user += user + 1
   print(user)
